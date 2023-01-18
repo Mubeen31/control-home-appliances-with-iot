6b97ca478455ad1dce1a9d0f6a49842278a86e8f
Mubeen31 <qs6272527@gmail.com>
2023-01-18 23:50:54 +0000

    first commit

---
+++ iot home appliances/iot home appliances.ino
@@ -0,0 +1,121 @@
+#include "DHT.h"
+#define DHTPIN D7
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "TALKTALK897C1F"
+#define WIFI_PASSWORD "QQYYT4AT"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyDJCuauXh0jJqbBMgc5u3qr0CKanP49rBU"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "iot-node-mcu-a3335-default-rtdb.firebaseio.com/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+//some important variables
+String lightValue, socketValue;
+bool signupOK = false;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+  pinMode(DHTPIN, INPUT);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(10000);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  float t = dht.readTemperature();
+  if (Firebase.ready() && signupOK ) {
+    if (Firebase.RTDB.getString(&fbdo, "/L1")) {
+      if (fbdo.dataType() == "string") {
+        lightValue = fbdo.stringData();
+        int a = lightValue.toInt();
+        Serial.println(a);
+        if (a == 1){
+          digitalWrite(D0,HIGH);
+        }else{
+          digitalWrite(D0,LOW);
+        }
+      }
+    }
+    else {
+      Serial.println(fbdo.errorReason());
+    }
+    if (Firebase.RTDB.getString(&fbdo, "/L2")) {
+      if (fbdo.dataType() == "string") {
+        socketValue = fbdo.stringData();
+        int b = socketValue.toInt();
+        Serial.println(b);
+        if (b == 1){
+          digitalWrite(D1,HIGH);
+        }else{
+          digitalWrite(D1,LOW);
+        }
+      }
+    }
+    else {
+      Serial.println(fbdo.errorReason());
+    }
+    if (Firebase.RTDB.setFloat(&fbdo, "DHT/temperature", t)){
+       Serial.print("Temperature: ");
+       Serial.println(t);
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }    
+  }
+  delay(10000);
+}
